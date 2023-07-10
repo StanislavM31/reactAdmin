a1284dd2375ec07a36f0acc7bc5ae6239d2aaea8
Stanislav Minko <minkostas@gmail.com>
2023-07-10 12:58:51 +0300

    reactAdmin: getContact async

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 console.log("hello admin!");
 
 const url = "http://3.65.149.62/test-api/auth/login";
-const urlGetContact= 'http://3.65.149.62/test-api/contacts';
-let jwt;
+const urlGetContact = "http://3.65.149.62/test-api/contacts";
 
-async function authorization(link){
+async function authorization(link) {
   let data = {
-    "email": "test@nyblecraft.com",
-    "password": "12345678qQ"
+    email: "test@nyblecraft.com",
+    password: "12345678qQ",
   };
 
   let response = await fetch(link, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
   let result = await response.json();
-  sessionStorage.setItem('res', result.accessToken);
-  console.log( sessionStorage.getItem("res"));
+  sessionStorage.setItem("res", result.accessToken);
+  console.log(sessionStorage.getItem("res"));
 }
 
-async function getContact(token){
+async function getContact(token) {
   let response = await fetch(urlGetContact, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      "Authorization": `Bearer ${token}`
-    }
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 
   let result = await response.json();
   console.log(result);
-
 }
-let promise1 = getContact(authorization(url));
-let promise2 = (getContact(sessionStorage.getItem("res")));
 
-
+async function main() {
+  await authorization(url);
+  let token = sessionStorage.getItem("res");
+  await getContact(token);
+}
+main();
 
