fd416f49629c9007d11a796241cd11bb717428af
Stanislav Minko <minkostas@gmail.com>
2023-09-05 03:12:43 +0300

    :intermediate commit

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-console.log("hello admin!");
+console.log("hello currencies!");
+require axios from "axios"
 
-const url = "http://3.65.149.62/test-api/auth/login";
-const urlGetContact = "http://3.65.149.62/test-api/contacts";
+/* ==== */
+ async function showCurrencies(){
 
-async function authorization(link) {
-  let data = {
-    email: "test@nyblecraft.com",
-    password: "12345678qQ",
-  };
+        const result = await axios.get('https://www.nbrb.by/API/ExRates/Currencies');
+        console.log(result);
+        console.log(result.data);
+      }
+/* ==== */
+/* async function main() {
 
-  let response = await fetch(link, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  let result = await response.json();
-  sessionStorage.setItem("res", result.accessToken);
-  console.log(sessionStorage.getItem("res"));
-}
-
-async function getContact(token) {
-  let response = await fetch(urlGetContact, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  let result = await response.json();
-  console.log(result);
-}
-
-async function main() {
-  await authorization(url);
-  let token = sessionStorage.getItem("res");
-  await getContact(token);
+  await showCurrencies();
 }
 main();
-
+ */
+await showCurrencies()
